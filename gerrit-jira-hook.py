b214e63a301557e3958aeabf0138c8c495fe1be4
D3PSI <d3psigames@gmail.com>
2020-07-29 13:12:19 +0200

    Add config file and connect to Jira instance server, implement connection error handling

--- gerrit-jira-hook.py
+++ gerrit-jira-hook.py
@@ -4,14 +4,35 @@
         Version:        1.0
 '''
 import os
+import requests
+import configparser
 import logging as log
-from jira import JIRA
+import jira
 
 
 log_path = os.path.join(os.path.dirname(__file__), 'hooks.log')
-file = open(log_path, 'a+').close()
-FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+FORMAT = '%(asctime)-12s %(levelname)-8s %(message)s'
 log.basicConfig(format=FORMAT, filename=log_path, level=log.DEBUG)
+
+config_path = os.path.join(os.path.dirname(__file__), 'gerrit-jira-hook.config')
+config = configparser.RawConfigParser()
+config.read(config_path)
+
+JIRA_URL = config.get('gerrit-jira-hook', 'jira_url')
+JIRA_USER = config.get('gerrit-jira-hook', 'jira_user')
+JIRA_PASS = config.get('gerrit-jira-hook', 'jira_pass')
+
+
+def init():
+    try:
+        open(log_path, 'a+').close()
+    except PermissionError:
+        print('Permission Error creating/opening log at "' + log_path + '"')
+        raise
+
+
+def find_issue_identifier(commit_msg):
+    return ''
 
 
 def jira_hook():
@@ -21,7 +42,26 @@ def jira_hook():
     a commit-message. The issue-id is extracted from the commig-message by 
     running a regular expression over it and seeing if it matches anything.
     '''
-    log.debug('Executing Jira comment hook...')
+    init()
+    log.debug('Executing gerrit-jira-hook...')
+    log.debug('Connecting to Jira-instance running at "' + JIRA_URL + '"...')
+    try:
+        jira_instance = jira.JIRA(JIRA_URL, basic_auth=(JIRA_USER, JIRA_PASS), logging=True)
+    except (requests.exceptions.ConnectionError, jira.exceptions.JIRAError) as e:
+        log.error('Connection to "' + JIRA_URL + '" as user "' + JIRA_USER
+            + '" failed')
+        exit(1)
+    except requests.exceptions.ConnectTimeout:
+        log.error('Connection to "' + JIRA_URL + '" as user "' + JIRA_USER
+            + '" timed out')
+        exit(1)
+    issue_id = find_issue_identifier('')
+    if issue_id != '':
+        log.info('Logging into "' + JIRA_URL + '" with user "' + JIRA_USER
+            + '"...')
+    else:
+        log.debug('No issue-id found, exiting gracefully...')
+        exit(0)
 
 
 if __name__ == "__main__":
