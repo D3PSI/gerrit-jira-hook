7a5d1409cfcbe533f66b65a36bb0a3dc2ed855ff
D3PSI <d3psigames@gmail.com>
2020-07-29 14:34:10 +0200

    Parse command line hook arguments

--- gerrit-jira-hook.py
+++ gerrit-jira-hook.py
@@ -4,8 +4,10 @@
         Version:        1.0
 '''
 import os
+import sys
 import requests
 import configparser
+import getopt
 import logging as log
 import jira
 
@@ -22,10 +24,60 @@ JIRA_URL = config.get('gerrit-jira-hook', 'jira_url')
 JIRA_USER = config.get('gerrit-jira-hook', 'jira_user')
 JIRA_PASS = config.get('gerrit-jira-hook', 'jira_pass')
 
+PROJECTS = config.get('gerrit-jira-hook', 'gerrit_projects')
+
+def show_usage():
+    print('\nNormal hook usage: ' + sys.argv[0] + ' --action [new|merged|abandoned] --change <change id> --commit <git hash> --change-url <url to change> --branch <branch name>')
+
 
 def init():
     try:
         open(log_path, 'a+').close()
+        if len(sys.argv) < 2:
+            show_usage()
+            exit()
+        necessary = ['action=', 'change=', 'change-url=', 'commit=',
+                    'project=', 'branch=', 'uploader=', 'patchset=',
+                    'abandoner=', 'reason=', 'submitter=', 'kind=',
+                    'is-draft=', 'change-owner=', 'newrev=', 'topic=']
+        optlist, args = getopt.getopt(sys.argv[1:], '', necessary)
+        id = ''
+        url = ''
+        hash = ''
+        who = '' 
+        topic = ''
+        for o, a in optlist:
+            if o == '--change':
+                id = a
+                log.debug("[DEBUG] Optlist Change-ID: " + id)
+            elif o == '--change-url':
+                url = a
+                log.debug("[DEBUG] Optlist Change-URL: " + url)
+            elif o == '--commit':
+                hash = a
+                log.debug("[DEBUG] Optlist Commit-ID (Hash): " + hash)
+            elif o == '--action':
+                what = a
+                log.debug("[DEBUG] Optlist action: " + what)
+            elif o == '--uploader':
+                who = a
+                log.debug("[DEBUG] Optlist uploader (who): " + who)
+            elif o == '--submitter':
+                who = a
+                log.debug("[DEBUG] Optlist submitter (who): " + who)
+            elif o == '--abandoner':
+                who = a
+                log.debug("[DEBUG] Optlist abandoner (who): " + who)
+            elif o == '--branch':
+                branch = a
+                log.debug("[DEBUG] Optlist branch: " + branch)
+            elif o == '--topic':
+                topic = a
+                log.debug("[DEBUG] Optlist topic: " + topic)
+        if (len(what) > 0 and len(id) > 0):
+            jira_hook(what, id, hash, url, who, branch, topic)
+        else:
+            show_usage()
     except PermissionError:
         print('Permission Error creating/opening log at "' + log_path + '"')
         raise
@@ -35,7 +87,11 @@ def find_issue_identifier(commit_msg):
     return ''
 
 
-def jira_hook():
+def generate_comment():
+    return ''
+
+
+def jira_hook(what, id, hash, url, who, branch, topic):
     '''
     The main function for the Gerrit-hook to post a comment on a JIRA-ticket 
     with the Change-Id and other information whenever an issue-id is detected in
@@ -55,14 +111,14 @@ def jira_hook():
         log.error('Connection to "' + JIRA_URL + '" as user "' + JIRA_USER
             + '" timed out')
         exit(1)
-    issue_id = find_issue_identifier('')
+    issue_id = find_issue_identifier(topic)
     if issue_id != '':
-        log.info('Logging into "' + JIRA_URL + '" with user "' + JIRA_USER
-            + '"...')
+        log.debug('Generating comment for Issue "' + issue_id + '"...')
+        comment = generate_comment()
     else:
         log.debug('No issue-id found, exiting gracefully...')
         exit(0)
 
 
 if __name__ == "__main__":
-    jira_hook()
+    init()
